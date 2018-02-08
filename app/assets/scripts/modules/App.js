0e691adf13685398efc408f65a79bf28c6681a33
Chris <krzysztof.naprawa.onet.com.pl>
2018-02-08 10:40:24 +0100

    JDFJFDJD

--- app/assets/scripts/modules/App.js
+++ app/assets/scripts/modules/App.js
@@ -1,8 +1,10 @@
 
 import Mobilemenu from "./Mobilemenu";
 import RevealOnScroll from "./RevealOnScroll";
+import StickyHeader from "./StickyHeader";
 import $ from "jquery";
 
 var mobilemenu=new Mobilemenu();
 var reveal=new RevealOnScroll($(".feature-item"),"85%");
 var reveall=new RevealOnScroll($(".testimonial"),"60%");
+var sticky=new StickyHeader();
