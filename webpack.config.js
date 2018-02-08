5517321a8bb8d86bb9f406ae03a51897761133be
Chris <krzysztof.naprawa.onet.com.pl>
2018-02-08 20:42:09 +0100

    Lazyloading

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,13 @@
 var path=require('path');
 
 module.exports={
-    entry:"./app/assets/scripts/modules/App.js",
+    entry:{
+        App:"./app/assets/scripts/modules/App.js",
+        Vendor:"./app/assets/scripts/modules/Vendor.js"
+    },
     output:{
         path:path.resolve(__dirname, "./app/temp/scripts"),
-        filename: "App.js",
+        filename: "[name].js",
     },
     module:{
         loaders: [
